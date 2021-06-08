e0965563271ba37d99c2c49a799e5b25f9555f74
Levente Wolf <wlevi2002@gmail.com>
2021-06-08 10:25:40 +0200

    Create outputHandler

---
+++ src/handlers/outputHandler/outputHandler.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "../../gamemaster/gameMaster.h"
+
+void show_mode_title(char *mode) {
+    printf("\n" PADDING "--%s--" PADDING "\n", mode);
+}
+
+void show_modes() {
+    printf(PADDING "--Main Menu--" PADDING "\n");
+    printf("1. Two player     (Player vs Player)\n");
+    printf("2. One player     (Player vs Computer)\n");
+    printf("3. Computer Fight (Computer vs Computer)\n");
+    printf("4. Practice       (Player vs Player N games)\n");
+    printf("0. Exit\n");
+}
+
+void show_victory(char *name) {
+    printf("\n" PADDING "----%s has just won the game!---\n", name);
+    printf(PADDING "      --Congratulations!--\n\n");
+}
+
+void show_draw() {
+    printf(PADDING "----GAME OVER----\n");
+    printf(PADDING "    --DRAW--\n\n");
+}
---
+++ src/handlers/outputHandler/outputHandler.h
@@ -0,0 +1,12 @@
+#ifndef CONNECTFOUR_OUTPUTHANDLER_H
+#define CONNECTFOUR_OUTPUTHANDLER_H
+
+void show_mode_title(char *mode);
+
+void show_modes();
+
+void show_victory(char *name);
+
+void show_draw();
+
+#endif //CONNECTFOUR_OUTPUTHANDLER_H
