2280840e4df9f6161cfdf502a6bb1366545f7a1b
Levente Wolf <wlevi2002@gmail.com>
2021-06-12 14:17:16 +0200

    Update debug printf

--- src/handlers/otherHeaders/debug.h
+++ src/handlers/otherHeaders/debug.h
@@ -2,9 +2,11 @@
 #define CONNECTFOUR_DEBUG_H
 
 #ifdef DEBUG
-#define debug_print(format, ...) printf(format, __VA_ARGS__)
+#define DEBUG 1
+#define debug_printf(format, ...) printf(format, __VA_ARGS__)
 #else
-#define debug_print(...)
+#define DEBUG 0
+#define debug_printf(...)
 #endif
 
 #endif //CONNECTFOUR_DEBUG_H
