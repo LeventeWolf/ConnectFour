2af3c7262ecc61683051775a92acd166354342e0
Levente Wolf <wlevi2002@gmail.com>
2021-06-17 23:07:49 +0200

    Move comment

--- src/board/board.c
+++ src/board/board.c
@@ -5,20 +5,6 @@
 #include "../handlers/otherHeaders/debug.h"
 #include "../handlers/modeHandler/modeHandler.h"
 
-//       -- Board --
-//
-//      0 1 2 3 4 5 6 7
-//   0 | | | | | | | | |
-//   1 | | | | | | | | |
-//   2 | | | | | | | | |
-//   3 | | | | | | | | |
-//   4 | | | | | | | | |
-//   5 | | | | | | | | |
-//   6 | | | | | | | | |
-//   7 | | | | | | | | |
-//     ─────────────────
-//     /1 2 3 4 5 6 7 8\
-
 char** board;
 
 void init_board() {
