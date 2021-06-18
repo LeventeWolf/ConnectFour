f104b0f329d6d42dc285e9d31db5289a97ddda0c
Levente Wolf <wlevi2002@gmail.com>
2021-06-18 14:28:41 +0200

    Add gameMaster tests

---
+++ src/gameMaster/utest-gameMaster.c
@@ -0,0 +1,17 @@
+#include <CUnit/CUnit.h>
+#include "utest-gameMaster.h"
+
+static void test_gameMaster() {
+    CU_FAIL("Not implemented yet")
+}
+
+CU_TestInfo gameMaster_tests[] = {
+        {"Testing board initialization", test_gameMaster},
+        CU_TEST_INFO_NULL
+};
+
+CU_SuiteInfo gameMaster_test_suites[] = {
+        {"GameMaster tests", NULL, NULL, NULL, NULL,
+         gameMaster_tests },
+        CU_SUITE_INFO_NULL
+};
--- src/main/utest.c
+++ src/main/utest.c
@@ -1,14 +1,15 @@
 #include "CUnit/Basic.h"
 #include "../board/utest-board.h"
+#include "../gameMaster/utest-gameMaster.h"
 #include <CUnit/CUnit.h>
 
 int main() {
-    if (CU_initialize_registry() != CUE_SUCCESS) {
-        return -1;
-    };
-    if (CU_register_suites(board_test_suites) != CUE_SUCCESS) {
-        return -1;
-    };
+    if (CU_initialize_registry() != CUE_SUCCESS) { return -1; }
+
+    if (CU_register_suites(board_test_suites) != CUE_SUCCESS) {return -1;}
+    if (CU_register_suites(gameMaster_test_suites) != CUE_SUCCESS) {return -1;}
+
+
     CU_basic_set_mode(CU_BRM_NORMAL);
     CU_basic_run_tests();
     CU_cleanup_registry();
